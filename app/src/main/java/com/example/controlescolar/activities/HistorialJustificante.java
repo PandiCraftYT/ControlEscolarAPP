31712d9ba9b8c1247f3e3eac735c522423c1d8c9
PandiCraftYT <149350482+PandiCraftYT@users.noreply.github.com>
2023-12-13 21:19:09 -0700

    FALTA API DE DESCARGAR JUSTIFICANTE

--- app/src/main/java/com/example/controlescolar/activities/HistorialJustificante.java
+++ app/src/main/java/com/example/controlescolar/activities/HistorialJustificante.java
@@ -3,11 +3,17 @@ package com.example.controlescolar.activities;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -15,14 +21,21 @@ import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.controlescolar.R;
 import com.example.controlescolar.api.ApiInterface;
 import com.example.controlescolar.api.RetrofitClient;
 import com.google.gson.JsonObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -79,58 +92,204 @@ public class HistorialJustificante extends AppCompatActivity {
     }
 
     private void mostrarDatosEnTabla(List<ApiInterface.Justificante> justificantes) {
-        // Obtener la referencia al layout de la tabla
-        TableLayout tableLayout = findViewById(R.id.tableLayout);
+        TableLayout tableLayout = findViewById(R.id.tableLayout); // Referencia al layout de la tabla
 
-        // Iterar sobre la lista de justificantes y agregar filas a la tabla
         for (ApiInterface.Justificante justificante : justificantes) {
             TableRow row = new TableRow(this);
             TableRow.LayoutParams rowLayoutParams = new TableRow.LayoutParams(
                     TableRow.LayoutParams.MATCH_PARENT,
                     TableRow.LayoutParams.WRAP_CONTENT
             );
-            rowLayoutParams.setMargins(0, 12, 0, 12); // Ajustar márgenes entre filas
+            rowLayoutParams.setMargins(0, 12, 0, 12);
             row.setLayoutParams(rowLayoutParams);
 
-            // Establecer un fondo para la fila
-            row.setBackgroundResource(R.drawable.background_info); // Reemplaza R.drawable.background_row con el nombre de tu fondo
+            int backgroundColor = Color.GRAY; // Color por defecto para el estado "PENDIENTE"
 
-            // Crear TextView para mostrar el valor del folio con salto de línea
+            switch (justificante.getEstado()) {
+                case "aceptado":
+                    backgroundColor = Color.GREEN;
+                    break;
+                case "rechazado":
+                    backgroundColor = Color.RED;
+                    break;
+                // Puedes agregar más casos según necesites
+            }
+
+            row.setBackgroundColor(backgroundColor); // Establecer color de fondo de la fila
+
             TextView folioTextView = new TextView(this);
             folioTextView.setText("FOLIO:\n" + justificante.getFolio());
-            folioTextView.setTextSize(20); // Tamaño del texto
-            folioTextView.setTextColor(Color.BLACK); // Color del texto
-            folioTextView.setPadding(0, 12, 24, 12); // Padding del texto
+            folioTextView.setTextSize(20);
+            folioTextView.setTextColor(Color.BLACK);
+            folioTextView.setPadding(0, 12, 24, 12);
             row.addView(folioTextView);
 
-            // Crear TextView para mostrar el valor de la fecha de solicitud con salto de línea
             TextView fechaSolicitudTextView = new TextView(this);
             fechaSolicitudTextView.setText("FECHA SOLICITUD:\n" + justificante.getFechaSolicitud());
-            fechaSolicitudTextView.setTextSize(20); // Tamaño del texto
-            fechaSolicitudTextView.setTextColor(Color.BLACK); // Color del texto
-            fechaSolicitudTextView.setPadding(0, 12, 24, 12); // Padding del texto
+            fechaSolicitudTextView.setTextSize(20);
+            fechaSolicitudTextView.setTextColor(Color.BLACK);
+            fechaSolicitudTextView.setPadding(0, 12, 24, 12);
             row.addView(fechaSolicitudTextView);
 
-            // Crear TextView para mostrar el valor del estado con salto de línea
             TextView estadoTextView = new TextView(this);
             estadoTextView.setText("ESTADO:\n" + justificante.getEstado());
-            estadoTextView.setTextSize(20); // Tamaño del texto
-            estadoTextView.setTextColor(Color.BLACK); // Color del texto
-            estadoTextView.setPadding(0, 12, 24, 12); // Padding del texto
+            estadoTextView.setTextSize(20);
+            estadoTextView.setTextColor(Color.BLACK);
+            estadoTextView.setPadding(0, 12, 24, 12);
             row.addView(estadoTextView);
 
-            // Agregar la fila a la tabla
-            tableLayout.addView(row);
+            tableLayout.addView(row); // Agregar fila a la tabla
 
-            // Agregar un espacio en blanco al final de cada fila para separar visualmente los datos
             View separatorView = new View(this);
             TableRow.LayoutParams separatorParams = new TableRow.LayoutParams(
                     TableRow.LayoutParams.MATCH_PARENT,
-                    20 // Altura del espacio en blanco (puedes ajustar este valor según la separación deseada)
+                    20
             );
             separatorView.setLayoutParams(separatorParams);
-            tableLayout.addView(separatorView);
+            tableLayout.addView(separatorView); // Agregar separador visual
+
+            if (justificante.getEstado().equals("aceptado")) {
+                row.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(HistorialJustificante.this);
+                        builder.setTitle("Descargar Archivo")
+                                .setMessage("¿Quieres descargar este archivo?")
+                                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        descargarJustificante(justificante.getFolio(), justificante.getFechaSolicitud());
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .show();
+                    }
+                });
+            }
         }
+    }
+
+
+
+    // Método para descargar un justificante
+    private void descargarJustificante(String folio, String fechaSolicitud) {
+        JsonObject jsonBody = new JsonObject();
+        jsonBody.addProperty("folio", folio);
+        jsonBody.addProperty("fecha_solicitud", fechaSolicitud);
+
+        Call<ResponseBody> call = apiInterface.descargarJustificante(jsonBody); // Reemplaza con el método correspondiente de tu API para descargar justificantes
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    if (responseBody != null) {
+                        try {
+                            boolean writtenToDisk = saveJustificanteToDisk(responseBody, folio + ".pdf");
+                            if (writtenToDisk) {
+                                showToast("Justificante descargado y guardado");
+
+                                // Abrir el archivo PDF automáticamente después de descargarlo
+                                openDownloadedPDFFile(folio + ".pdf");
+                            } else {
+                                showToast("Error al guardar el justificante");
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            showToast("Error al guardar el justificante: " + e.getMessage());
+                        }
+                    }
+                } else {
+                    Log.e(TAG, "Error en la respuesta del servidor al descargar justificante");
+                    showToast("Error al descargar justificante");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e(TAG, "Error en la llamada a la API al descargar justificante", t);
+                showToast("Error al descargar justificante. Verifica tu conexión a Internet.");
+            }
+        });
+    }
+    private void openDownloadedPDFFile(String filename) {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), filename);
+
+        if (file.exists()) {
+            Uri fileUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", file);
+
+            Intent target = new Intent(Intent.ACTION_VIEW);
+            target.setDataAndType(fileUri, "application/pdf");
+            target.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+            Intent intent = Intent.createChooser(target, "Abrir archivo");
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                showToast("No se pudo abrir el archivo. Instala una aplicación para visualizar PDFs.");
+            }
+        } else {
+            showToast("El archivo no existe.");
+        }
+    }
+    // Método para guardar el justificante en el dispositivo
+    private boolean saveJustificanteToDisk(ResponseBody body, String filename) throws IOException {
+        try {
+            File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            File file = new File(directory, filename);
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                byte[] fileReader = new byte[4096];
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(file);
+
+                while (true) {
+                    int read = inputStream.read(fileReader);
+                    if (read == -1) {
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+                    fileSizeDownloaded += read;
+                }
+
+                outputStream.flush();
+                return true;
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Método para mostrar un Toast con el mensaje especificado
+    private void showToast(String message) {
+        Toast.makeText(HistorialJustificante.this, message, Toast.LENGTH_SHORT).show();
     }
 
     public void Atras(View view) {
